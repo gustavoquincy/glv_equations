2dd9e1609f222c5cecd165f84af84fa6a37e4d0f
gustavoquincy <yuankaile@gmail.com>
2023-07-03 11:03:08 +0800

    add thrust and matrix type

--- glv_equation.hpp
+++ glv_equation.hpp
@@ -2,12 +2,23 @@
 #include <vector>
 #include <cmath>
 
-#include <omp.h>
 #include <boost/numeric/odeint.hpp>
-#include <boost/numeric/odeint/external/openmp/openmp.hpp>
+#include <boost/numeric/odeint/external/thrust/thrust.hpp>
 
-typedef std::vector<double_t> state_type;
-typedef runge_kutta_dopri5<state_type, double_t, state_type, double_t, openmp_range_algebra> stepper_type;
+#include <boost/random/mersenne_twister.hpp>
 
-state_type equations(state_type, size_t, std::vector<double_t> /* growth rate */, std::vector<double_t> /* Sigma */, std::vector<std::vector<double_t>> /* interaction matrix */, double_t /* dilution */);
+#include <thrust/device_vector.h>
+#include <thrust/reduce.h>
+#include <thrust/functional.h>
+
+using namespace std;
+using namespace boost::numeric::odeint;
+
+typedef double_t value_type;
+typedef thrust::device_vector< value_type > state_type;
+typedef thrust::device_vector< device_vector < value_type >> matrix_type;
+typedef runge_kutta_dopri5< state_type, value_type, state_type, double_t, openmp_range_algebra> stepper_type;
+
+
+state_type equations(state_type, size_t, state_type  /* growth rate */, state_type  /* Sigma */,  matrix_type /* interaction matrix */, value_type /* dilution */);
 
